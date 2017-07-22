3e676420465fa72b97ef9ccc7efd95b92ae6ccd7
aikin <1@aikin.me>
2017-07-22 23:31:10 +0800

    aikin: extract init machine

--- observer-pattern/src/main/java/com/refactor/observerpattern/WeatherData.java
+++ observer-pattern/src/main/java/com/refactor/observerpattern/WeatherData.java
@@ -12,21 +12,26 @@ public class WeatherData {
     private final SeedingMachine seedingMachine;
     private final ReapingMachine reapingMachine;
     private final WateringMachine wateringMachine;
+    private List<IMachineObserver> machineObservers;
 
     public WeatherData(SeedingMachine seedingMachine, ReapingMachine reapingMachine, WateringMachine wateringMachine) {
         this.seedingMachine = seedingMachine;
         this.reapingMachine = reapingMachine;
         this.wateringMachine = wateringMachine;
+
+        this.machineObservers = initMachineObservers();
     }
 
     public void measurementsChanged(int temp, int humidity, int windPower) {
+        machineObservers.forEach(o -> o.update(temp, humidity, windPower));
+    }
+
+    private List<IMachineObserver> initMachineObservers() {
         SeedingMachineObserver seedingMachineObserver = new SeedingMachineObserver(seedingMachine);
         ReapingMachineObserver reapingMachineObserver = new ReapingMachineObserver(reapingMachine);
         WateringMachineObserver wateringMachineObserver = new WateringMachineObserver(wateringMachine);
 
-        List<IMachineObserver> machineObservers = Arrays.asList(seedingMachineObserver, reapingMachineObserver, wateringMachineObserver);
-
-        machineObservers.forEach(o -> o.update(temp, humidity, windPower));
+        return Arrays.asList(seedingMachineObserver, reapingMachineObserver, wateringMachineObserver);
     }
 
 }
