98ceda5f7f7ce866e3ea4357f80cc84f6ed2e8d8
aikin <1@aikin.me>
2018-07-12 13:37:42 +0800

    feat(MakingMehtodCallsSimplerAddParamter): replace.constructorwith.factory.method

---
+++ refactoring-techniques/making-method-calls-simpler/src/main/java/me/aikin/refactoring/making/method/calls/simpler/replace/constructorwith/factory/method/Employee.java
@@ -0,0 +1,12 @@
+package me.aikin.refactoring.making.method.calls.simpler.replace.constructorwith.factory.method;
+
+public class Employee {
+    private int _type;
+    static int ENGINEER = 0;
+    static int SALESMAN = 1;
+    static int MANAGER = 2;
+
+    public Employee(int type) {
+        _type = type;
+    }
+}
